c9c5a83eb5e96fcfb64b25d6c43af418df84ebd9
Blake <blake.duckworth@outlook.com>
2021-02-01 22:29:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,6 +55,7 @@ echo "Done."
 # Setup
 echo -n "Creating server directory..."
 sudo mkdir $MC_HOME -p
+sudo chown $USER $MC_HOME
 
 if [ ! -d $MC_HOME ]; then
     exit_early "Could not create $MC_HOME directory - aborting."
@@ -64,8 +65,8 @@ echo " Done."
 # Download other scripts
 echo -n "Getting utility scripts..."
 cd $MC_HOME
-wget -bq "https://raw.githubusercontent.com/00Duck/JavaMinecraftScripts/main/warn.sh"
-wget -bq "https://raw.githubusercontent.com/00Duck/JavaMinecraftScripts/main/versioncheck.sh"
+wget -q "https://raw.githubusercontent.com/00Duck/JavaMinecraftScripts/main/warn.sh"
+wget -q "https://raw.githubusercontent.com/00Duck/JavaMinecraftScripts/main/versioncheck.sh"
 echo "Done."
 
 echo -n "Enter amount of memory for server to use. Ex: 256M, 2G, 4G. (Default 2G): "
@@ -81,7 +82,7 @@ echo ""
 # Install Service
 echo -n "Installing Service..."
 EXECUTE="/usr/bin/screen -dm -S minecraft-server bash -c 'java -Xms$mem -Xmx$mem -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar server.jar nogui'"
-echo "
+SERVICE="
 [Unit]
 Description=Vanilla Minecraft Server
 
@@ -95,14 +96,14 @@ Type=forking
 
 [Install]
 WantedBy=multi-user.target
-" | sudo tee -a /etc/systemd/system/multi-user.target.wants/minecraft-server.service
+" | sudo dd of=/etc/systemd/system/multi-user.target.wants/minecraft-server.service
 echo " Done."
 
 # Install Minecraft
 echo -n "Installing Vanilla Minecraft Server..."
 server_page=$(curl -L https://www.minecraft.net/en-us/download/server)
 mcs_url=$(grep -o "https://.*server\.jar" <<<$server_page)
-wget $mcs_url
+wget -q $mcs_url
 echo " Done."
 
 # Install Crontabs
@@ -123,16 +124,16 @@ echo "Starting Minecraft Server."
 sudo systemctl start minecraft-server.service
 
 # FINN
-echo "Your Vanilla Minecraft Server has finished installing."
-echo "\nCommon commands:"
-echo "   Start Server: sudo systemctl start minecraft-server.service"
-echo "   Stop Server: sudo systemctl stop minecraft-server.service"
-echo "   Restart Server: sudo systemctl restart minecraft-server.service"
-echo "   Server status: systemctl status minecraft-server.service"
-echo "\nYour server is running on a separate screen. Type `screen -r` to resume the background screen session. Please note that, in order to do this, you must be echoged in using screen with the same user that is running the service. To exit screen, type `ctrl + a` and then `ctrl + d`.\n"
-echo `Your server will shutdown nightly with a 10 minute warning every minute. 
+echo -e "Your Vanilla Minecraft Server has finished installing.\n
+Common commands:
+   Start Server: sudo systemctl start minecraft-server.service
+   Stop Server: sudo systemctl stop minecraft-server.service
+   Restart Server: sudo systemctl restart minecraft-server.service
+   Server status: systemctl status minecraft-server.service
+\nYour server is running on a separate screen. Type `screen -r` to resume the background screen session. Please note that, in order to do this, you must be logged in using screen with the same user that is running the service. To exit screen, type `ctrl + a` and then `ctrl + d`.\n
+\nYour server will shutdown nightly with a 10 minute warning every minute. 
 By default, the countdown starts at 2:20am and the server reboots at 2:30am. This 
 is configured to help prevent memory corruption issues when running the server on 
-non-ecc, consumer hardware. You can modify this behavior by typing sudo su - to switch to root, followed by crontab -e.`
-echo "\nYou can automatically check for and download new versions of the server by running versioncheck.sh in $MC_HOME"
-echo "Enjoy. :)"
+non-ecc, consumer hardware. You can modify this behavior by typing sudo su - to switch to root, followed by crontab -e.
+\nYou can automatically check for and download new versions of the server by running versioncheck.sh in $MC_HOME
+\nEnjoy. :)"
