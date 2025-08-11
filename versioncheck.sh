9c86f8c5856bec75295390853343e3d262be3c9f
Blake Duckworth <blake.duckworth@outlook.com>
2025-08-10 20:47:26 -0400

    update version checker to account for new page

--- versioncheck.sh
+++ versioncheck.sh
@@ -13,29 +13,27 @@
 ##################################################################################
 
 #download the latest page
-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
-echo -n "Downloading latest server page..."
-server_page=$(curl -A "$agent" -sL https://www.minecraft.net/en-us/download/server)
+agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+echo -n "Checking latest version..."
 
 #get version and put in version file
 file_version=$(cat version)
-new_version=$(grep -o -m 1 minecraft_server.*\.jar <<<$server_page | sed "s/minecraft_server.//g;s/\.jar//g")
-
-echo "Done"
+new_version=$(curl -A "$agent" -sL "https://net-secondary.web.minecraft-services.net/api/v1.0/download/latest" | jq '.result' | tr -d '"')
 
 if [ "$file_version" != "$new_version" ]; then
     echo -n "Version mismatch - Stopping server..."
     sudo systemctl stop minecraft_server.service
-    echo "Done"
+    echo "Server stopped."
 
-    echo -n "Backing up old jar as server.jar.old..."
+    echo -n "Backing up old jar..."
+    rm server.jar.old
     mv server.jar server.jar.old
-    echo "Done"
+    echo "server.jar moved to server.jar.old"
 
 	echo -n "Downloading latest version..."
 	#grab the download URL and download
-	url=$(grep -o "https://.*server\.jar" <<<$server_page)
-	wget -q $url
+	download_link=$(curl -A "$agent" -sL "https://net-secondary.web.minecraft-services.net/api/v1.0/download/links" | jq '.result | .links[] | select(.downloadType=="serverJar") | .downloadUrl' | tr -d '"')
+	wget -q $download_link
     echo "Done"
 
 	#Update version file to latest version
